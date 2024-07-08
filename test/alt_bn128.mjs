1ee1f0fb322afeb83beade0108a8ae171370fad4
vimwitch <jchancehud@gmail.com>
2024-07-07 22:37:49 -0700

    ava tests

--- test/alt_bn128.mjs
+++ test/alt_bn128.mjs
@@ -1,40 +1,41 @@
 import { buildPoseidon } from 'circomlibjs'
 import { poseidonT3 } from '../src/alt_bn128/t3.mjs'
 import crypto from 'crypto'
+import test from 'ava'
 
-// test random inputs against the circomlibjs implementation
-const circomPoseidon = await buildPoseidon()
-const count = 10000
+test('vs circomlibjs', async t => {
+  // test random inputs against the circomlibjs implementation
+  const circomPoseidon = await buildPoseidon()
+  const count = 10000
 
-const NS_PER_SEC = 1e9
-const NS_PER_MSEC = 1e6
+  const NS_PER_SEC = 1e9
+  const NS_PER_MSEC = 1e6
 
-const times = []
+  const times = []
 
-for (let x = 0; x < count; x++) {
-  if (x % 1000 === 0 && x > 0) console.log(x)
-//   const inputCount = 1 + (x % 16)
-  const inputCount = 2
-  const inputs = []
-  for (let y = 0; y < inputCount; y++) {
-    inputs.push(
-      '0x' +
-        crypto.randomBytes(Math.floor(1 + 10 * Math.random())).toString('hex')
-    )
+  for (let x = 0; x < count; x++) {
+    // if (x % 1000 === 0 && x > 0) console.log(x)
+  //   const inputCount = 1 + (x % 16)
+    const inputCount = 2
+    const inputs = []
+    for (let y = 0; y < inputCount; y++) {
+      inputs.push(
+        '0x' +
+          crypto.randomBytes(Math.floor(1 + 10 * Math.random())).toString('hex')
+      )
+    }
+    const circomOutput = BigInt(circomPoseidon.F.toString(circomPoseidon(inputs)))
+    const time = process.hrtime()
+  //   const out = poseidon[`poseidon${inputs.length}`](inputs)
+    const out = poseidonT3(inputs)
+    const diff = process.hrtime(time)
+    times.push((diff[0] * NS_PER_SEC + diff[1]) / NS_PER_MSEC)
+    t.is(circomOutput, out)
   }
-  const circomOutput = BigInt(circomPoseidon.F.toString(circomPoseidon(inputs)))
-  const time = process.hrtime()
-//   const out = poseidon[`poseidon${inputs.length}`](inputs)
-  const out = poseidonT3(inputs)
-  const diff = process.hrtime(time)
-  times.push((diff[0] * NS_PER_SEC + diff[1]) / NS_PER_MSEC)
-  if (circomOutput !== out) throw new Error('Hash output mismatch')
-}
 
-{
-  const sum = times.reduce((acc, v) => acc + v, 0)
-  const avg = sum / times.length
-  console.log(`Average ${avg} ms per hash`)
-}
-
-console.log('\npassed')
+  {
+    const sum = times.reduce((acc, v) => acc + v, 0)
+    const avg = sum / times.length
+    console.log(`Average ${avg} ms per alt_bn128 hash`)
+  }
+})
--- test/bls12_381.mjs
+++ test/bls12_381.mjs
@@ -1,15 +1,33 @@
 import * as snarkjs from 'snarkjs'
 import path from 'path'
 import assert from 'assert'
+import crypto from 'crypto'
 import { poseidonT3 } from '../src/bls12_381/t3.mjs'
-
-const { publicSignals } = await snarkjs.groth16.fullProve(
-    { inputs: [1n, 1n] },
-    './out/poseidon_test_js/poseidon_test.wasm',
-    './out/poseidon_test_final.zkey'
-)
-console.log(publicSignals)
-assert.equal(publicSignals[0], poseidonT3([1n, 1n]))
+import test, { registerCompletionHandler } from 'ava'
 
 // Needed because snarkjs causes the process to hang
-process.exit(0)
+registerCompletionHandler(() => {
+	process.exit()
+});
+
+test('circom impl', async t => {
+    const count = 100
+    for (let x = 0; x < count; x++) {
+        // if (x % 10 === 0 && x > 0) console.log(x)
+        const inputCount = 2
+        const inputs = []
+        for (let y = 0; y < inputCount; y++) {
+            inputs.push(
+                '0x' +
+                crypto.randomBytes(Math.floor(1 + 10 * Math.random())).toString('hex')
+            )
+        }
+        const { publicSignals } = await snarkjs.groth16.fullProve(
+            { inputs },
+            './out/poseidon_test_js/poseidon_test.wasm',
+            './out/poseidon_test_final.zkey'
+        )
+        const out = poseidonT3(inputs)
+        t.is(publicSignals[0], out.toString())
+    }
+})
